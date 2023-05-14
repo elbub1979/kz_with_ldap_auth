f04bc9904db35ff714ea7e5e500ea046dfae0ec9
elbub <ilya.leuh09@gmail.com>
2023-05-15 00:08:56 +0300

    add def short_name, add short name to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,19 @@ class User < ApplicationRecord
 
   before_save :ldap_before_save
 
+  def short_name
+    name_array = name.split(' ')
+    lname = name_array[0]
+    fname = "#{name_array[1][0].upcase}." unless name_array[1].nil?
+    pname = "#{name_array[2][0].upcase}." unless name_array[2].nil?
+    fname.nil? && pname.nil? ? lname : "#{lname} #{fname}#{pname}"
+  end
+
   private
 
   def ldap_before_save
     self.name = Devise::LDAP::Adapter.get_ldap_param(self.username, "name").try(:first) || username
     self.email = Devise::LDAP::Adapter.get_ldap_param(self.username, "mail").try(:first) ||
-      "#{username}@#{Rails.application.credentials.devise.domain_name}"
+                 "#{username}@#{Rails.application.credentials.devise.domain_name}"
   end
 end
