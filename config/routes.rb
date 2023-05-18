ae5915177cafe274d6bbfa602e18365c24c22815
elbub <ilya.leuh09@gmail.com>
2023-05-19 00:06:21 +0300

    add custom registration routes in devise; fixed errors when creating a user without devise; added code in user show view; replacing the form creation helper in devise user registration view; replacing the devise user registration helper in application view; added code in user partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
   get 'home', to: 'static_pages#home'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions', users: 'users', registrations: 'users/registrations'
+  devise_for :users, skip: :registration, controllers: {
+    sessions: 'users/sessions', registrations: 'users/registrations'
   }
 
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
     get '/users/sign_out', to: 'users/sessions#destroy'
+    get '/users/registration/new', to: 'users/registrations#new'
+    post '/users/registration', to: 'users/registrations#create'
 
-    resources :users
+
     # get '/users/new', to: 'users#new'
     # get '/users/:id', to: 'users#show'
     # post '/users/:id', to: 'users#create'
   end
+
+  resources :users
+
 
   root to: 'static_pages#home'
 end
