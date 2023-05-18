5f6e056beb68accb4c8264a91ffc4689b0767760
elbub <ilya.leuh09@gmail.com>
2023-05-18 21:20:36 +0300

    add User controller with new, create and related methods; views new,, partial users form; user routes in users devise scope

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   get 'home', to: 'static_pages#home'
+
   devise_for :users, controllers: {
-    sessions: 'users/sessions', registrations: 'users/registrations'
+    sessions: 'users/sessions', users: 'users', registrations: 'users/registrations'
   }
+
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
     get '/users/sign_out', to: 'users/sessions#destroy'
+
+    resources :users
+    # get '/users/new', to: 'users#new'
+    # get '/users/:id', to: 'users#show'
+    # post '/users/:id', to: 'users#create'
   end
+
   root to: 'static_pages#home'
 end
