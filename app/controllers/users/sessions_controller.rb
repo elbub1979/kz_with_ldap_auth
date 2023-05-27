d9d7dfa8e28310b21cae70dee4b14754ed43cbb1
elbub <ilya.leuh09@gmail.com>
2023-05-28 00:12:47 +0300

    add comments in devise session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,16 +10,28 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
+    # приведение в нижний регистр данных, полученных из формы
     username = params['user']['username'].downcase
+    # поиск пользователя в лоакльной базе данных
     possible_user = User.find_by(username: username)
 
+    # выбор стратегии аутентификации
+    #
+    # логика следующая: при наличии пользователя в локальной базе данных происходит аутентийикация пользователя
+    # из локальной базы данных, если нет в локальной базе данных, аутентийикаия пользователя происходит из
+    # контроллера домена
+    #
+    # проверка наличия пользователя в локальной базе данных и валидности пароля из локальной базы данных
     if possible_user&.valid_password?(params[:user][:password])
+      # аутентификация в локальной базе данных
       self.resource = warden.authenticate!(:database_authenticatable)
 
+      # следующие три строчки кода - копипаста с stackoverflow
       sign_in(resource_name, resource)
       yield resource if block_given?
       respond_with resource, location: after_sign_in_path_for(resource)
     else
+      # аутентификация ldap
       super
     end
   end
