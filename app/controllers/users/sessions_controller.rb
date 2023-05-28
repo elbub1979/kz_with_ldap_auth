9aa8eb777473aa31c1c6b13cbfca4b7a3a41e481
elbub <ilya.leuh09@gmail.com>
2023-05-28 22:57:17 +0300

    refactoring devise session controller#create

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -23,6 +23,7 @@ class Users::SessionsController < Devise::SessionsController
     #
     # проверка наличия пользователя в локальной базе данных и валидности пароля из локальной базы данных
     if possible_user&.valid_password?(params[:user][:password])
+
       # аутентификация в локальной базе данных
       self.resource = warden.authenticate!(:database_authenticatable)
 
@@ -31,9 +32,21 @@ class Users::SessionsController < Devise::SessionsController
       yield resource if block_given?
       respond_with resource, location: after_sign_in_path_for(resource)
     else
+
+      # проверка валидности данных учётной записи из полученной от пользователя формы
+      # в контроллере домена
+      if Devise::LDAP::Adapter.valid_credentials?(params[:user][:username], params[:user][:password])
+        possible_user.update(user_params)
+      end
+
       # аутентификация ldap
-      super
-    end
+      self.resource = warden.authenticate!(:ldap_authenticatable)
+
+      # следующие три строчки кода - копипаста с stackoverflow
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+      ы    end
   end
 
   # DELETE /resource/sign_out
@@ -46,5 +59,16 @@ class Users::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+  end
+
+  def user_params
+    accessible = [
+      :name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    ]
+    params.require(:user).permit(accessible)
   end
 end
