9bcd5e9e5c7de5196a943dea8d9aa6d8796846b3
elbub <ilya.leuh09@gmail.com>
2023-06-04 23:15:55 +0300

    add pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   before_action :authenticate_user!
 
   rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
