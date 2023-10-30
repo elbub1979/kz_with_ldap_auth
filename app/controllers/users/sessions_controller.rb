88b029bcb7685f22180a18e68d757c0088041c18
elbub1979 <ilya.leuh09@gmail.com>
2023-10-30 23:50:24 +0300

    refactoring session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,71 +25,71 @@ class Users::SessionsController < Devise::SessionsController
     # и, если валидный, обновлетс в локальной базе данных.
 
     # если пользователя нет в локальной базе данных и в доменном контроллере, уведомляем его о нвозможности аутентификации
-    if !possible_user && Devise::LDAP::Adapter.valid_credentials?(username, password)
-      redirect_to sign_in_path
-      set_flash_message(:alert, :signed_in_db)
+    unless possible_user
+      #  и нет доменном контроллере, уведомляем его о нвозможности аутентификации
+      if !Devise::LDAP::Adapter.valid_credentials?(username, password)
+        redirect_to sign_in_path
+        set_flash_message(:alert, :signed_in_db)
+
+      # если есть в доменном контроллере
+      elsif Devise::LDAP::Adapter.valid_credentials?(username, password)
+        # создаем нового пользователя
+        possible_user = User.new
+
+        # присваиваем соответствующии атриьуты, полученые из формы
+        possible_user.username = username
+        possible_user.password = password
+
+        # если имя отсутствует, получаем из контроллера домена или используем логин в качестве имени
+        unless possible_user.name.present?
+          possible_user.name = Devise::LDAP::Adapter.get_ldap_param(username, 'name').try(:first) ||
+            username
+        end
+
+        # если электронная почта отсутствует, получаем из контроллера домена или создаем из логина
+        # и имени домена
+        unless possible_user.email.present?
+          possible_user.email = Devise::LDAP::Adapter.get_ldap_param(username, 'mail').try(:first) ||
+            "#{username}@#{Rails.application.credentials.devise.domain_name}"
+        end
+
+        # получаем список групп, в которых состоит пользователь
+        groups = Devise::LDAP::Adapter.get_ldap_param(username, 'memberof')
+
+        # прерывем выполнение метода, если пользователь не состоит в группах
+        return unless groups
+
+        # получаем группы из массива в установленном формате (убрать узлы из состава названия группы)
+        groups.map! do |group|
+          group.match(/CN=[а-яА-Яa-zA-Z]+\s*[а-яА-Яa-zA-Z]*/).to_s.split('=')[1]
+        end
+
+        # установливаем роль администратора, если пользователь состоит в группе доменных администраторов
+        # выносим список групп для проверки членства в credentials
+        if groups.any? { |group| Rails.application.credentials.dig(:ldap, :admin_groups).include?(group) }
+          possible_user.role = 1
+        end
+
+        # сохраняем обновалённые данные пользователя
+        possible_user.save!
+      end
     end
 
+    # если пользователь существует
+    if possible_user
+      # но учетные данные не валидны в локальной базе данных, но валидны в контроллере домена, обновляем до валидных данных
+      possible_user.update!(user_params) if !possible_user.valid_password?(params[:user][:password]) && Devise::LDAP::Adapter.valid_credentials?(username, password)
 
-    # если пользователя нет в локальной базе данных, но есть в доменном контроллере
-    if !possible_user && Devise::LDAP::Adapter.valid_credentials?(username, password)
+      # если валидны, то аутентифицируемся
+      if possible_user.valid_password?(params[:user][:password])
+        self.resource = warden.authenticate!(:database_authenticatable)
+        set_flash_message(:notice, :signed_in_db)
 
-      # создаем нового пользователя
-      possible_user = User.new
-
-      # присваиваем соответствующии атриьуты, полученые из формы
-      possible_user.username = username
-      possible_user.password = password
-
-      # если имя отсутствует, получаем из контроллера домена или используем логин в качестве имени
-      unless possible_user.name.present?
-        possible_user.name = Devise::LDAP::Adapter.get_ldap_param(username, 'name').try(:first) ||
-                             username
+        # следующие три строчки кода - копипаста с stackoverflow
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource, location: after_sign_in_path_for(resource)
       end
-
-      # если электронная почта отсутствует, получаем из контроллера домена или создаем из логина
-      # и имени домена
-      unless possible_user.email.present?
-        possible_user.email = Devise::LDAP::Adapter.get_ldap_param(username, 'mail').try(:first) ||
-                              "#{username}@#{Rails.application.credentials.devise.domain_name}"
-      end
-
-      # получаем список групп, в которых состоит пользователь
-      groups = Devise::LDAP::Adapter.get_ldap_param(username, 'memberof')
-
-      # прерывем выполнение метода, если пользователь не состоит в группах
-      return unless groups
-
-      # получаем группы из массива в установленном формате (убрать узлы из состава названия группы)
-      groups.map! do |group|
-        group.match(/CN=[а-яА-Яa-zA-Z]+\s*[а-яА-Яa-zA-Z]*/).to_s.split('=')[1]
-      end
-
-      # установливаем роль администратора, если пользователь состоит в группе доменных администраторов
-      # выносим список групп для проверки членства в credentials
-      if groups.any? { |group| Rails.application.credentials.dig(:ldap, :admin_groups).include?(group) }
-        possible_user.role = 1
-      end
-
-      # сохраняем обновалённые данные пользователя
-      possible_user.save!
-    end
-
-    # обновляем пользовтеля если его нет в локальной базе данных, но существует в контроллеретдомена
-    possible_user.update!(user_params) if possible_user&.!valid_password?(params[:user][:password]) &&
-                                                          Devise::LDAP::Adapter.valid_credentials?(username, password)
-
-    # проверяем наличие пользователя в локальной базе данных и валидность пароля из локальной базы данных
-    if possible_user&.valid_password?(params[:user][:password])
-
-      # аутентификация в локальной базе данных
-      self.resource = warden.authenticate!(:database_authenticatable)
-      set_flash_message(:notice, :signed_in_db)
-
-      # следующие три строчки кода - копипаста с stackoverflow
-      sign_in(resource_name, resource)
-      yield resource if block_given?
-      respond_with resource, location: after_sign_in_path_for(resource)
     end
   end
 
